eb7a0f082f42e918ec9f6b364b8b4f65aba7d5dd
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-11 19:12:11 -0800

    added Link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -10,13 +11,14 @@ class Blog extends Component {
                 <header className='Blog'>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path='/' exact render={() => <div>Home</div> } /> */}
                 <Route path='/' exact component={Posts} />
+                <Route path='/new-post' exact component={NewPost} />
             </div>
         );
     }
