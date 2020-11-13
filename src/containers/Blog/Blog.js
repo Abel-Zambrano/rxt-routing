b2aa13768b7eb4e81875434b2ee50e3fc6d4c146
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-11-13 06:16:55 -0800

    added Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import FullPost from './FullPost/FullPost'; 
 
 class Blog extends Component {
@@ -17,9 +17,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact component={Posts} />
-                <Route path='/new-post' exact component={NewPost} />
-                <Route path='/:id' exact component={FullPost} />
+                <Switch>
+                    <Route path='/' exact component={Posts} />
+                    <Route path='/new-post' exact component={NewPost} />
+                    <Route path='/:id' exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
